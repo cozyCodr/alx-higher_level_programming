4ea08fea18d441e50a23f7b68d01ee8e5fac0a38
Bright Londa <bright.cxc@gmail.com>
2022-05-31 00:20:28 +0200

    fixed output

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# 5 lines of code
-print(f"{str[39:66]} {str[-22:-18]} {str[0:6]}")
+print(f"{str[39:66]}"
+      f" {str[-22:-18]} {str[0:6]}")
