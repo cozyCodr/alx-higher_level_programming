75eeba1bfc6efd49077cc32b6c63c8e86922bfd7
Bright Londa <bright.cxc@gmail.com>
2022-05-31 22:08:23 +0200

    fixed negative output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,7 @@ if last_num > 5:
     print(f"Last digit of {number} is {last_num} and is greater than 5")
 elif last_num == 0:
     print(f"Last digit of {number} is {last_num} and is zero")
-elif last_num < 6 and last_num != 0:
+elif last_num < 6 and last_num != 0 and number > 0:
     print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
+elif last_num < 6 and last_num != 0 and number < 0:
+    print(f"Last digit of {number} is -{last_num} and is less than 6 and not 0")
