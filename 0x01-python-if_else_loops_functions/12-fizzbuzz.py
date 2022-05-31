6a1fdb5cdbf3da989f6a0f08e154965ee85c5acc
Bright Londa <bright.cxc@gmail.com>
2022-06-01 00:16:47 +0200

    hot fix.2

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     for i in range(1, 100):
         if i % 3 == 0:
             if i % 5 == 0:
-                print(" FizzBuzz", end=" ")
+                print("FizzBuzz", end=" ")
             else:
                 print("Fizz", end=" ")
         elif i % 5 == 0:
