6cc279d9798df6de4df2cd9295d1e54c2d863d78
Bright Londa <bright.cxc@gmail.com>
2022-06-03 00:21:55 +0200

    fixed output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,11 @@ args = sys.argv
 
 
 def main():
-    if len(args) <= 1:
+    if len(args) < 1:
         print("0 arguments.")
+    elif len(args) == 2:
+        print("1 argument:")
+        print("1: {}".format(args[1]))
     else:
         print("{} arguments:".format(len(args) - 1))
         for index, arg in enumerate(args):
