2ca4e1a895485db7b066d64e5bf9fd77ed0da227
Bright Londa <bright.cxc@gmail.com>
2022-06-02 23:18:49 +0200

    fixed pycodestyle error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-
 a = 1
 b = 2
 
+
 def main():
-    print(f"{a} + {b} = {add(a,b)}") 
+    print(f"{a} + {b} = {add(a,b)}")
+
 
 if __name__ == "__main__":
     main()
