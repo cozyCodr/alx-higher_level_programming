77fd148f43bc005587bc3f01ffc78e4787379a4f
Bright Londa <bright.cxc@gmail.com>
2022-06-06 12:30:33 +0200

    fixed output error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,9 +5,5 @@ removes all instances of c and C in String
 
 
 def no_c(my_string):
-    new_string = list(my_string)
-    for i in new_string:
-        index = new_string.index(i)
-        if i == 'c' or i == 'C':
-            new_string.pop(index)
+    new_string = [x for x in my_string if x != 'c' and x != 'C']
     return "".join(new_string)
