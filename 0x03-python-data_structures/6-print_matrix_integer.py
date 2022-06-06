13046d2e34a40501c66a62f1e28f62de1e5cb07e
Bright Londa <bright.cxc@gmail.com>
2022-06-06 13:45:44 +0200

    hot fix .1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,19 +5,9 @@ prints a 2 dimensional matrix
 
 
 def print_matrix_integer(matrix=[[]]):
-    if isinstance(matrix, list):
-        if len(matrix) == 1:
-            for inner in matrix:
-                for x in inner:
-                    print("{:d}".format(x), end="")
-            print("$")
-        else:
-            for inner in matrix:
-                for x in inner:
-                    if inner.index(x) == (len(inner) - 1):
-                        print("{:d}$".format(x))
-                    else:
-                        print("{:d}".format(x), end=" ")
-    elif isinstance(matrix, str):
-        new_matrix = [*matrix]
-        print("{}$".format("".join(new_matrix)))
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][column]), end=" ")
+            if column != len(matrix[row]) - 1:
+                print("{}".format(""), end=" ")
+        print()
