3d6a6cca649c944536bc23e1bd4a7acdb58b233f
Bright Londa <bright.cxc@gmail.com>
2022-06-06 14:45:38 +0200

    fixed pycodestyle errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-""" 
+"""
 add the contents of 2 tuples
 """
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        return tuple([tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]])
+        return tuple([int(tuple_a[0]) + int(tuple_b[0]),
+                     int(tuple_a[1]) + int(tuple_b[1])])
     elif len(tuple_a) >= 2 and len(tuple_b) < 2:
         if len(tuple_b) == 1:
-            return tuple([tuple_a[0] + tuple_b[0], tuple_a[1] + 0])
+            return tuple([int(tuple_a[0]) + int(tuple_b[0]),
+                          int(tuple_a[1]) + 0])
         if len(tuple_b) == 0:
             return tuple_a
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        return tuple([tuple_a[0] + tuple_b[0], 0])
+        return tuple([int(tuple_a[0]) + int(tuple_b[0]), 0])
     elif len(tuple_b) >= 2 and len(tuple_a) < 2:
         if len(tuple_a) == 1:
-            return tuple([tuple_a[0] + tuple_b[0], tuple_b[1]])
+            return tuple([int(tuple_a[0]) + int(tuple_b[0]), int(tuple_b[1])])
         if len(tuple_a) == 0:
-                return tuple_b
+            return tuple_b
