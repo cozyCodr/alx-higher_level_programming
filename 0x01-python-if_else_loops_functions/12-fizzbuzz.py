038c4a7f835d8021b8961a57b284d6ef7b37d7f5
cozyCodr <brightlonda.scholar@gmail.com>
2022-09-20 22:26:45 +0200

    fixed spacing

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,14 +5,11 @@ def fizzbuzz():
 
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            fizzbuzz += " FizzBuzz"
+            fizzbuzz += "FizzBuzz "
         elif i % 3 == 0:
-            fizzbuzz += " Fizz"
+            fizzbuzz += "Fizz "
         elif i % 5 == 0:
-            fizzbuzz += " Buzz"
+            fizzbuzz += "Buzz "
         else:
-            if i == 1:
-                fizzbuzz += str(i)
-            else:
-                fizzbuzz = fizzbuzz + " " + str(i)
+            fizzbuzz = fizzbuzz + str(i) + " "
     print(fizzbuzz)
