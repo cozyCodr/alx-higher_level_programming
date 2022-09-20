9661671320990331e3955666daa366f44e828828
cozyCodr <brightlonda.scholar@gmail.com>
2022-09-20 21:26:37 +0200

    fixed linting error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,6 +6,6 @@ for x in range(10):
         if x > y or x == y:
             continue
         if (x == 8 and y == 9):
-            print("{}{}".format(x,y))
+            print("{}{}".format(x, y))
         else:
-            print("{}{}".format(x,y), end=", ")
+            print("{}{}".format(x, y), end=", ")
