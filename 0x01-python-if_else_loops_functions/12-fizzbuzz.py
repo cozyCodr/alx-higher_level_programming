cc78a77d853ad393bbf1994800585ded17f92bdc
cozyCodr <brightlonda.scholar@gmail.com>
2022-09-20 22:19:46 +0200

    hot fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,14 @@
 
 def fizzbuzz():
     fizzbuzz = ""
+    
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-                fizzbuzz += " FizzBuzz"
+            fizzbuzz += " FizzBuzz"
         elif i % 3 == 0:
             fizzbuzz += " Fizz"
         elif i % 5 == 0:
             fizzbuzz += "Buzz"
         else:
             fizzbuzz = fizzbuzz + " " + str(i)
+    print(fizzbuzz)
