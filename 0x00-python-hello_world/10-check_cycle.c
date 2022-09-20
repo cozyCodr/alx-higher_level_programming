1ed357fc49413d04d960de10f438fa09fdcfed30
cozyCodr <brightlonda.scholar@gmail.com>
2022-09-20 15:09:37 +0200

    added check cycle function

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
-#include <lists.h>
+#include "lists.h"
 /**
  * check_cycle - checks for a cycle in a linked list
  * @list: pointer to head of list
  * Return: 1 if cycle is found and 0 if no cyle is found
  */
-int check_cycle(listint_t *list){
+int check_cycle(listint_t *list)
+{
 
 	listint_t *fast;
 	listint_t *slow;
 
+	if (list == NULL)
+		return (0);
+
 	fast = list->next;
 	slow = list;
 
-	while (slow != null)
+	while (slow != NULL && fast->next != NULL && fast != NULL)
 	{
-		if (slow == fast && fast != null)
+		if (slow == fast)
 			return (1);
-		if (fast->next->next != null)
-			fast = fast->next->next;
+		fast = fast->next->next;
 		slow = slow->next;
 	}
+
 	return (0);
 }
