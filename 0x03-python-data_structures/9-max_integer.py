f3fa9046a2b396dc43f39e8520a9ff6c9180e1db
cozyCodr <brightlonda.scholar@gmail.com>
2022-09-27 08:52:33 +0200

    first commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    largest = 0
+
+    if len(my_list) == 0:
+        return None
+    for number in my_list:
+        if number >= count:
+            largest = number
+    return largest
